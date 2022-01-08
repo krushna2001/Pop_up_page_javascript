103e132aeb7c483f8170329e96b988ccae5fbd54
krushna2001 <krushnadh01@gmail.com>
2022-01-08 10:23:52 +0530

    first commit

---
+++ code.js
@@ -0,0 +1,12 @@
+/*function help()
+{
+    //alert("Working");
+    let image = document.getElementById("image");
+    image.src = "https://www.google.com/search?q=images&rlz=1C1CHBD_enIN930IN930&sxsrf=AOaemvI8Bh6Y_zIVA8vQgOMzqnRuLpNQLQ:1641525050783&tbm=isch&source=iu&ictx=1&fir=L8xfQakH9a8tJM%252CBa_eiczVaD9-zM%252C_%253Bv1A5vQPzRgfCGM%252C7T8fKkixbRnWvM%252C_%253B2eIb5BjhQiiZzM%252CnBiD9BWYMB87aM%252C_%253BLK6S_eMkLDVwQM%252CB51x0PBR9KNzvM%252C_%253BydaEQ15j2QXL8M%252CwJ5ddLGQliC7WM%252C_%253BITUG1uFAV1aQ9M%252CX9GBbDAacv-dWM%252C_%253BOFjLbX408Yt9iM%252CjMPx2dPKbYbCVM%252C_%253BeXUC-3WyVcZa-M%252CfcNz05h1Ca2eyM%252C_%253BtTplitM2kjOQtM%252C-UStXW0dQEx4SM%252C_%253BarFfSjMu_GX7sM%252CUkcvm3PybD5jEM%252C_%253B2DNOEjVi-CBaYM%252CAOz9-XMe1ixZJM%252C_%253BsSAWficq0VlQLM%252CG9GbNX6HcZ2O_M%252C_%253BcQmPOi5AjFldKM%252ClscPDp2wO7k-wM%252C_%253Bi0x7moJaCtF5XM%252Cfb09TtLq3UJOWM%252C_&vet=1&usg=AI4_-kTKqYkY4_mjwE3RrLOvAxyiSNrUwA&sa=X&ved=2ahUKEwi_0KSs1Z71AhXSLqYKHcA3BwsQ9QF6BAgCEAE#imgrc=L8xfQakH9a8tJM"
+ 
+ 
+            document.getElementById("btnId")
+                    .style.display = "none";
+    document.getElementById('dothis').innerHTML = "krushna krushna krushna krushna"
+}*/
+ 
